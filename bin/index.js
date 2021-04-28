3ccc73f1e931a97874a4149b57994dc20dae4060
Junior Teixeira <juniorteixeira1805@ufrn.edu.br>
2021-04-28 00:36:05 -0300

    Adicionado variaveis de ambiente

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors') // essa biblioteca serve para gerenciar quais outros dominios podem acessar minha API
 const requireDir = require('require-dir')
+require('dotenv/config');
 
 const PORT = 8080
 
@@ -10,8 +11,11 @@ const app = express();
 app.use(express.json())
 app.use(cors()) // Aqui pode ser configurado a segurança e os dominios perrmitidos
 
+const path = require('path')
+app.use(express.static(path.join(__dirname, '../public')))
+
 //-- iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true })
+mongoose.connect(process.env.BD_TESTES, { useUnifiedTopology: true })
 
 //--Importando models
     //- O require dir é uma biblioteca para importa pasta em um arquivo
