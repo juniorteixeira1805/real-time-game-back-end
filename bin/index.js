935b5cd2dc8662d22b3e624a7cabba6e0900f3eb
Junior Teixeira <juniorteixeira1805@ufrn.edu.br>
2021-04-28 10:50:49 -0300

    Adicionado body-parse e mudança na variavel de ambiente

--- bin/index.js
+++ bin/index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors') // essa biblioteca serve para gerenciar quais outros dominios podem acessar minha API
 const requireDir = require('require-dir')
-require('dotenv/config');
+require('dotenv/config')
+const bodyParser = require('body-parser')
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 //-- Iniciando o app
-const app = express();
+const app = express()
+app.use(bodyParser.json()) // O body-parser é um módulo capaz de converter o body da requisição para vários formatos. Um desses formatos é json.
+app.use(bodyParser.urlencoded({ extended: false}))
 app.use(express.json())
 app.use(cors()) // Aqui pode ser configurado a segurança e os dominios perrmitidos
 
@@ -24,4 +27,4 @@ requireDir('../src/Models')
 //--Rotas
 app.use('/', require('../src/Routes/routes'))
 
-app.listen(PORT, ()=>console.log('API its working || localhost:8080'))
+app.listen(PORT, ()=>console.log('API its working'))
