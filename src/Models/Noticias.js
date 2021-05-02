b0dfca2573c3fad9030f0437ee0d1a127c8db2e4
Junior Teixeira <juniorteixeira1805@ufrn.edu.br>
2021-05-02 13:52:43 -0300

    Adicionado o Model noticias

---
+++ src/Models/Noticias.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+// mongoose-paginate é utilizado para auxiliar na listagem dos models
+const mongoosePaginate = require('mongoose-paginate')
+
+
+const NoticiastSchema = new mongoose.Schema({
+    dateCreater: {
+        type: Date,
+        default: Date.now
+    },
+    dateNoticia: {
+        type: String,
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description : {
+        type: String,
+    },
+    images: [{
+        link: {
+            type: String,
+        },
+        authorImage: {
+            type: String,
+        }
+    }],
+})
+
+formatarData = async function(dateCreater) {
+    let data = new Date(dateCreater)
+    let dia = data.getDate()
+    if(dia<10) dia = `0${dia}`
+    let mes = parseInt(data.getMonth()) + 1
+    if(mes<10) mes = `0${mes}`
+    let ano = data.getFullYear()
+
+    return `${dia}/${mes}/${ano}`
+}
+
+NoticiastSchema.pre('save', async function(next){
+    try {
+        this.dateNoticia = await formatarData(this.dateCreater)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+// Definido o pluglin para poder utilizar a função paginate
+NoticiastSchema.plugin(mongoosePaginate)
+
+mongoose.model('noticias', NoticiastSchema)
